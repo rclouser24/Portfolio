c2eb4ecd67f051d43959aafa14fe9ca7d76fe86c
Clouser <clouser24ryan@gmail.com>
2025-01-14 13:54:11 -0500

    added router to main.jsx

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+import App from './App';
+import ErrorPage from './pages/ErrorPage';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
+import AboutPage from './pages/AboutPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: '/Blog',
+        element: <Blog />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
